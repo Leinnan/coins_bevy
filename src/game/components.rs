8e883cac8cfe78e9e4f1641b7f747d360500fc12
Piotr Siuszko <siuszko@zoho.com>
2023-10-30 19:43:53 +0100

    cleanup

---
+++ src/game/components.rs
@@ -0,0 +1,47 @@
+use bevy::prelude::*;
+use bevy::prelude::{Component, Resource};
+
+#[derive(Component)]
+pub struct TextChanges;
+
+// Used to help identify coin controlled by player
+#[derive(Component)]
+pub struct PlayerControlled;
+
+#[derive(Component)]
+pub struct PointerArrow;
+
+#[derive(Resource, Reflect)]
+pub struct GameplaySettings {
+    pub min_force: Vec2,
+    pub max_force: Vec2,
+}
+
+#[derive(Resource, Reflect, Default)]
+pub struct GameplayProgress {
+    pub touches: i32,
+    pub moves: i32,
+    pub is_inside_end_place: bool,
+}
+
+impl GameplaySettings {
+    pub fn get_shoot_strength(&self, distance: f32) -> Option<f32> {
+        if distance < self.min_force.x {
+            return None;
+        }
+        let strength = distance.min(self.max_force.x) / self.max_force.x
+            * (self.max_force.y - self.min_force.y)
+            + self.min_force.y;
+        eprintln!("Distance {distance} with strength: {strength}");
+        Some(strength)
+    }
+}
+
+impl Default for GameplaySettings {
+    fn default() -> Self {
+        GameplaySettings {
+            min_force: Vec2::new(25.0, 1.0),
+            max_force: Vec2::new(150.0, 200.0),
+        }
+    }
+}
---
+++ src/input/mod.rs
@@ -0,0 +1,112 @@
+use crate::game::components::{GameplayProgress, GameplaySettings, PlayerControlled};
+
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+use bevy_rapier2d::prelude::*;
+
+#[derive(Event)]
+pub struct AimingEvent {
+    pub player_pos: Vec2,
+    pub direction: Vec2,
+    pub strength: f32,
+}
+
+#[derive(Event)]
+pub struct AimingEndedEvent {
+    pub shoot: bool,
+}
+
+/// We store the world position of the mouse cursor here.
+#[derive(Resource, Default, Reflect)]
+pub struct MouseWorldPosition(Vec2);
+
+pub struct GameInputPlugin;
+
+/// Used to help identify our main camera
+#[derive(Component)]
+pub struct MainCamera;
+
+impl Plugin for GameInputPlugin {
+    fn build(&self, app: &mut App) {
+        app.register_type::<MouseWorldPosition>()
+            .init_resource::<MouseWorldPosition>()
+            .add_event::<AimingEndedEvent>()
+            .add_event::<AimingEvent>()
+            .add_systems(Update, player_input)
+            .add_systems(Update, my_cursor_system);
+    }
+}
+
+fn my_cursor_system(
+    mut mycoords: ResMut<MouseWorldPosition>,
+    // query to get the window (so we can read the current cursor position)
+    q_window: Query<&Window, With<PrimaryWindow>>,
+    // query to get camera transform
+    q_camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+) {
+    // get the camera info and transform
+    // assuming there is exactly one main camera entity, so Query::single() is OK
+    let (camera, camera_transform) = q_camera.single();
+
+    // There is only one primary window, so we can similarly get it from the query:
+    let window = q_window.single();
+
+    // check if the cursor is inside the window and get its position
+    // then, ask bevy to convert into world coordinates, and truncate to discard Z
+    if let Some(world_position) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+        .map(|ray| ray.origin.truncate())
+    {
+        mycoords.0 = world_position;
+    }
+}
+
+fn player_input(
+    buttons: Res<Input<MouseButton>>,
+    mouse_pos: Res<MouseWorldPosition>,
+    settings: Res<GameplaySettings>,
+    mut aim_event: EventWriter<AimingEvent>,
+    mut aim_event_2: EventWriter<AimingEndedEvent>,
+    mut ext_impulses: Query<(&mut ExternalImpulse, &Transform, &Velocity), With<PlayerControlled>>,
+    mut progress: ResMut<GameplayProgress>,
+) {
+    let released = buttons.just_released(MouseButton::Left);
+    if buttons.pressed(MouseButton::Left) || released {
+        let position = mouse_pos.0;
+        let (mut external, transform, velocity) = ext_impulses.single_mut();
+
+        if velocity.linvel.length() > 0.1 {
+            eprintln!("Still moving({}), skipping", velocity.linvel);
+            if released {
+                aim_event_2.send(AimingEndedEvent { shoot: false });
+            }
+            return;
+        }
+
+        let player_pos = Vec2::new(transform.translation.x, transform.translation.y);
+        let distance = position.distance(player_pos);
+        let strength = settings.get_shoot_strength(distance);
+        if strength.is_none() {
+            if released {
+                aim_event_2.send(AimingEndedEvent { shoot: false });
+            }
+            return;
+        }
+        let strength = strength.unwrap();
+        let dir = (position - player_pos).normalize();
+        if released {
+            eprintln!("{},{},{},{}", position, player_pos, dir, strength);
+            aim_event_2.send(AimingEndedEvent { shoot: true });
+            external.impulse = dir * strength;
+            external.torque_impulse = 0.3;
+            progress.moves += 1;
+        } else {
+            aim_event.send(AimingEvent {
+                player_pos,
+                strength,
+                direction: dir,
+            });
+        }
+    }
+}
